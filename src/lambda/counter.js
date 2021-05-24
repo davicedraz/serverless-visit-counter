1965fe6b2036614806ce7743cc362086637ba275
Davi Cedraz <davioler@gmail.com>
2021-05-24 14:14:17 -0300

    feat: implements counterAPI integration

---
+++ src/lambda/counter.js
@@ -0,0 +1,34 @@
+﻿'use strict';
+
+const incrementHit = async (event) => {
+  try {
+    const countapi = require('countapi-js');
+    await countapi.hit('davicedraz.com', 'visits');
+
+    return {
+      statusCode: 204,
+    };
+  } catch (error) {
+    return error;
+  }
+
+};
+
+const getVisits = async (event) => {
+  try {
+    const countapi = require('countapi-js');
+    const result = await countapi.get('davicedraz.com', 'visits');
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ result })
+    };
+  } catch (error) {
+    return error;
+  }
+};
+
+module.exports = {
+  getVisits,
+  incrementHit
+}
